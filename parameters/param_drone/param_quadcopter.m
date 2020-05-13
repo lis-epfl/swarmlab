897fbe42ef6799b967d7b510e551a2ca588f33b0
Enrica <enrica.s92@gmail.com>
2020-05-13 14:58:19 +0200

    Set param values for crazyflie

--- parameters/param_drone/param_quadcopter.m
+++ parameters/param_drone/param_quadcopter.m
@@ -1,8 +1,8 @@
-p_drone.mass = 0.468/sqrt(2.5);      % [kg]
-p_drone.Jx   = 0.004856;   % [kg * m2]
-p_drone.Jy   = 0.004856;   % [kg * m2]
-p_drone.Jz   = 0.008801;   % [kg * m2]
-p_drone.Jxz  = 0;          % [kg * m2]
+p_drone.mass = 0.025;      % [kg]
+p_drone.Jx   = 1.657171e-5;   % [kg * m2]
+p_drone.Jy   = 1.6655602e-5;   % [kg * m2]
+p_drone.Jz   = 2.9261652e-5;   % [kg * m2]
+p_drone.Jxz  = 0.718277e-6;    % [kg * m2]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute gammas
@@ -19,19 +19,19 @@ p_drone.gamma7 = ((p_drone.Jx-p_drone.Jy)*p_drone.Jx+p_drone.Jxz^2)/p_drone.gamm
 p_drone.gamma8 = p_drone.Jx/p_drone.gamma;
 
 % Aerodynamic coefficients
-p_drone.l_arm     = 0.225;            % [m] length of one arm
-p_drone.J_prop     = 3.357*10^(-5);   % [kg * m^2] inertia of each propeller
+p_drone.l_arm      = 0.03252691193;            % [m] length of one arm
+p_drone.J_prop     = 0;   % [kg * m^2] inertia of each propeller %TODO
 
-p_drone.k_omega       = 2000/2.5;         % [rad/s] max angular velocity for each motor
-p_drone.C_prop        = 2.98*10^(-6); % proportional coeff of propeller force
+p_drone.k_omega       = 3052;         % [rad/s] max angular velocity for each motor
+p_drone.C_prop        = 1.2819184e-8; % proportional coeff of propeller force
 
-p_drone.Ax            = 0.25;         % [kg/s]
-p_drone.Ay            = 0.25; 
-p_drone.Az            = 0.25; 
+p_drone.Ax            = 4*9.1785e-7;         % [kg/s]
+p_drone.Ay            = 4*9.1785e-7; 
+p_drone.Az            = 4*9.1785e-7; 
 
 p_drone.drag_area     = 0.08;         % [m^2]
 
-p_drone.CD            = 1.14*10^(-7); % drag constant
+p_drone.CD            = p_drone.C_prop*0.005964552; % drag constant
 
 % p_drone.pn_T    = 0;  % initial North position
 % p_drone.pe_T    = 0;  % initial East position
@@ -59,7 +59,7 @@ p_drone.Va_T = 0;
 p_drone.altitude_take_off_zone = 5;
 
 % Values (for reference DJI Phantom 3: 15m/s)
-p_drone.Va0 = 5;        % m/s
+p_drone.Va0 = 3;        % m/s
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -72,50 +72,52 @@ p_drone.ki_h = 0.5;
 
 % Roll torque hold controller
 omega_n_phi     = 4;
-p_drone.kp_phi        = p_drone.Jx*omega_n_phi^2;
-p_drone.kd_phi        = 2*KSI_OPT*omega_n_phi*p_drone.Jx;
+p_drone.kp_phi  = p_drone.Jx*omega_n_phi^2;
+p_drone.kd_phi  = 2*KSI_OPT*omega_n_phi*p_drone.Jx;
 
 % Pitch torque hold controller
-omega_n_theta   = 4;
-p_drone.kp_theta      = p_drone.Jy*omega_n_theta^2;
-p_drone.kd_theta      = 2*KSI_OPT*omega_n_theta*p_drone.Jy;
+omega_n_theta       = 4;
+p_drone.kp_theta    = p_drone.Jy*omega_n_theta^2;
+p_drone.kd_theta    = 2*KSI_OPT*omega_n_theta*p_drone.Jy;
 
 % Yaw torque hold controller
 omega_n_psi     = 3;
-p_drone.kp_psi        = p_drone.Jz*omega_n_psi^2;
-p_drone.kd_psi        = 2*KSI_OPT*omega_n_psi*p_drone.Jz;
+p_drone.kp_psi  = p_drone.Jz*omega_n_psi^2;
+p_drone.kd_psi  = 2*KSI_OPT*omega_n_psi*p_drone.Jz;
 
 % p_drone.kp_psi      = 1.75/100;
 % p_drone.kd_psi      = 6/100;
 
 % vd thrust hold controller
 omega_n_vd      = 5;
-p_drone.kp_vd         = -2*KSI_OPT*omega_n_vd*p_drone.mass;
-p_drone.ki_vd         = p_drone.Az-omega_n_vd^2*p_drone.mass;
+p_drone.kp_vd   = -2*KSI_OPT*omega_n_vd*p_drone.mass;
+p_drone.ki_vd   = p_drone.Az-omega_n_vd^2*p_drone.mass;
 
 % vn pitch hold controller
 omega_n_vn      = 1;
-uh              = p_drone.C_prop*p_drone.k_omega^2*4*0.31^2;
-p_drone.kp_vn         = (-2*KSI_OPT*omega_n_vn*p_drone.mass + p_drone.Ax)/uh;
-p_drone.ki_vn         = -(omega_n_vn^2*p_drone.mass)/uh;
+v_hover         = p_drone.mass * 9.81 / ...
+                (4 * p_drone.C_prop * p_drone.k_omega^2);
+uh              = p_drone.C_prop*p_drone.k_omega^2*v_hover;
+p_drone.kp_vn   = (-2*KSI_OPT*omega_n_vn*p_drone.mass + p_drone.Ax)/uh;
+p_drone.ki_vn   = -(omega_n_vn^2*p_drone.mass)/uh;
 
 % ve roll hold controller
 omega_n_ve      = 1;
-p_drone.kp_ve         = (2*KSI_OPT*omega_n_ve*p_drone.mass - p_drone.Ay)/uh;
-p_drone.ki_ve         = (omega_n_ve^2*p_drone.mass)/uh;
+p_drone.kp_ve   = (2*KSI_OPT*omega_n_ve*p_drone.mass - p_drone.Ay)/uh;
+p_drone.ki_ve   = (omega_n_ve^2*p_drone.mass)/uh;
 
 % ad thrust hold controller
-tau_ad          = 0.1;
+tau_ad                = 0.1;
 p_drone.kp_ad         = 0;
 p_drone.ki_ad         = (p_drone.Az-p_drone.mass/tau_ad);
 
 % an pitch hold controller
-tau_an          = 0.2;
+tau_an                = 0.2;
 p_drone.kp_an         = 0;
 p_drone.ki_an         = (tau_an*p_drone.Ax-p_drone.mass)/(tau_an*uh);
 
 % ae roll hold controller
-tau_ae          = 0.2;
+tau_ae                = 0.2;
 p_drone.kp_ae         = 0;
 p_drone.ki_ae         = (tau_ae*p_drone.Ay+p_drone.mass)/(tau_ae*uh);
 
