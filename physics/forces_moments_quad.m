1e30d978a5bd3403bdd064928bb63e2dfb964ad1
Enrica <enrica.s92@gmail.com>
2020-10-20 17:58:38 +0200

    Estimation: start implementation

--- physics/forces_moments_quad.m
+++ physics/forces_moments_quad.m
@@ -1,27 +1,28 @@
 function out = forces_moments_quad(x, delta, wind, ~, p_drone, p_sim, p_physics)
 
-% FORCES_MOMENTS - Computes the forces and moments acting on the UAV.
+% FORCES_MOMENTS - Compute the forces and moments acting on the UAV, in 
+% the body frame.
 %
 % Inputs:
-%   x     - state variables
-%   delta - actuators values (angles,...)
-%   wind  - wind parameters
-%   P     - general parameters
+%   x: state variables
+%   delta: actuators values (angles,...)
+%   wind: wind parameters
+%   p_drone: drone parameters
+%   p_sim: simulation parameters
+%   p_physics: physics parmeters
+%
 % Outputs:
-%   out 
-%       F     - forces
-%       M     - moments
-%       Va    - airspeed
-%       alpha - angle of attack
-%       beta  - sideslip angle
-%       wind  - wind vector in the inertial frame
+%   out:
+%       F: forces in the body frame
+%       M: moments in the body frame
+%       Va: airspeed
+%       alpha: angle of attack
+%       beta: sideslip angle
+%       wind: wind vector in the inertial frame
 %
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Variables and Inputs
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Variables and Inputs
 
 % UAV velocity wrt inertial frame in body frame
 v_xyz      = x(4:6);
@@ -45,10 +46,7 @@ d4 = delta(4); % fourth
 Rbi = Rb2i(phi,theta,psi);
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Wind computation
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Wind computation
 
 ws_ned  = wind(1:3);  % steady wind in NED frame
 wg_xyz  = wind(4:6);  % gusts along body xyz axis
@@ -95,11 +93,7 @@ ca = cos(alpha);
 sa = sin(alpha);
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     Compute forces on quadcopter
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Compute forces on quadcopter
 
 weight = p_drone.mass * p_physics.gravity * [-sin(theta);...
     cos(theta)  * sin(phi);...
@@ -113,7 +107,7 @@ f_thrust = [0, 0, - p_drone.C_prop*p_drone.k_omega^2* (d1^2+d2^2+d3^2+d4^2)]';
 %         0        0       p_drone.Az];
 %     f_drag = - A*v_xyz;
 
-% non linear drag
+% Non-linear drag
 f_drag = -0.5 * p_physics.rho * Va * p_drone.drag_area .* va_xyz;
 
 % Total force
@@ -135,11 +129,9 @@ torque_aerodyn = [0, 0, 0]';
 torque_tot = torque_thrust + torque_aerodyn;
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     Create output
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Create output
 
 out = [f_tot; torque_tot; Va; alpha; beta; wn; we; wd];
+
 
 end
